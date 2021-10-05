1eb53cd42f7a041a62d418b22b1e8d11a2372b63
cuminsoda <44092199+sahil639@users.noreply.github.com>
2021-10-06 00:19:53 +0530

    update

--- sorting and searching/bubble_sort.py
+++ sorting and searching/bubble_sort.py
@@ -3,9 +3,12 @@
 #swapping in taken place at each iteration after one fixed position that position is ignored
 
 def bub_sort(num):
+    # if num is 7 then the range is from 6 to 0 at interval of -1 
     for i in range(len(num)-1,0,-1):
+    # traverses through whats left of i
         for j in range(i): 
            if num[j] > num[j+1]:
+               #swap time
                temp = num[j]
                num[j] = num[j+1]
                num[j+1] = temp
@@ -16,3 +19,6 @@ num = list(map(int,input("\nEnter the numbers : ").strip().split()))[:n]
 print(f"your unsorted list is : {num}")
 bub_sort(num)
 print(f"your sorted list is : {num}")
+
+
+# bubble sort ---> len-1 to 0 at -1 and swapping 
--- sorting and searching/quick_sort.py
+++ sorting and searching/quick_sort.py
@@ -1,8 +1,15 @@
+#The Quick Sort algorithm takes an item from the unsorted list and uses it as the 'pivot' or the item to compare the remainder of the items in the list. 
+# We do comparisons placing the items in lists according to if they are larger or smaller than the pivot value.
+
+
 def quick_sort(sequence):
+
     length = len(sequence)
+
     if length <= 1:
         return sequence
     else:
+        #removes last item as the 'pivot' or the item to compare the remainder of the items
         pivot = sequence.pop()
 
     items_greater = []
@@ -17,4 +24,8 @@ def quick_sort(sequence):
 
     return quick_sort(items_lower) + [pivot] + quick_sort(items_greater)
 
-print(quick_sort([5,6,7,8,9,8,7,6,5,6,7,8,9,0]))
+n = int(input("Enter the number of items in list :\n"))
+sequence = list(map(int, input("Enter the list you want to sort:\n").strip().split()))[:n]
+print(f"your unsorted list is: {sequence}")
+a= quick_sort(sequence)
+print(f"your sorted list using quick sort is: {a}")
